fc70573e4dff89cad940dbfb532d568398838ff5
Mehanari <102820106+Mehanari@users.noreply.github.com>
2023-08-27 14:10:42 +0300

    Made board validator and entry point stub (Game class).

--- castledice-game-logic-tests/ObjectCreationUtility.cs
+++ castledice-game-logic-tests/ObjectCreationUtility.cs
@@ -1,4 +1,7 @@
 ﻿using castledice_game_logic;
+using castledice_game_logic.BoardGeneration.CellsGeneration;
+using castledice_game_logic.BoardGeneration.ContentGeneration;
+using castledice_game_logic.GameConfiguration;
 using castledice_game_logic.GameObjects;
 using castledice_game_logic.Math;
 
@@ -22,6 +25,38 @@ public static class ObjectCreationUtility
     public static Cell GetCell()
     {
         return new Cell(new Vector2Int(0, 0));
+    }
+    
+    public static List<Player> GetPlayersList(int length)
+    {
+        List<Player> players = new List<Player>();
+        for (int i = 0; i < length; i++)
+        {
+            players.Add(GetPlayer());
+        }
+
+        return players;
+    }
+
+    public static BoardConfig GetDefaultBoardConfig(Player firstPlayer, Player secondPlayer)
+    {
+        var cellsGenerator = new RectCellsGenerator(10, 10);
+        var castlesSpawner = new CastlesSpawner(new Dictionary<Player, Vector2Int>()
+        {
+            {firstPlayer, new Vector2Int(0, 0)},
+            {secondPlayer, new Vector2Int(9, 9)}
+        });
+        var cellType = CellType.Square;
+        var boardConfig = new BoardConfig()
+        {
+            CellsGenerator = cellsGenerator,
+            ContentSpawners = new List<IContentSpawner>()
+            {
+                castlesSpawner
+            },
+            CellType = cellType
+        };
+        return boardConfig;
     }
     
     public static Player GetPlayer()
---
+++ castledice-game-logic/GameConfiguration/BoardConfig.cs
@@ -0,0 +1,11 @@
+﻿using castledice_game_logic.BoardGeneration.CellsGeneration;
+using castledice_game_logic.BoardGeneration.ContentGeneration;
+
+namespace castledice_game_logic.GameConfiguration;
+
+public struct BoardConfig
+{
+    public List<IContentSpawner> ContentSpawners;
+    public ICellsGenerator CellsGenerator;
+    public CellType CellType;
+}
