52f0dab347d132d3ca88e73c958ab2167080bcd0
Mehanari <102820106+Mehanari@users.noreply.github.com>
2023-09-10 21:40:13 +0300

    Wrote tests for GameOverChecker.

---
+++ castledice-game-logic-tests/GameOverCheckerTests.cs
@@ -0,0 +1,62 @@
+﻿using castledice_game_logic;
+
+namespace castledice_game_logic_tests;
+using static ObjectCreationUtility;
+
+
+public class GameOverCheckerTests
+{
+    [Fact]
+    public void IsGameOver_ShouldReturnFalse_IfTwoOrMoreCastlesOwnersOnBoard()
+    {
+        var board = GetFullNByNBoard(10);
+        var firstCastle = GetCastle(GetPlayer());
+        var secondCastle = GetCastle(GetPlayer());
+        board[1, 1].AddContent(firstCastle);
+        board[4, 4].AddContent(secondCastle);
+        var checker = new GameOverChecker(board);
+        
+        Assert.False(checker.IsGameOver());
+    }
+    
+    [Fact]
+    public void IsGameOver_ShouldReturnTrue_IfOnlyOneCastlesOwnerOnBoard()
+    {
+        var board = GetFullNByNBoard(10);
+        var castlesOwner = GetPlayer();
+        var firstCastle = GetCastle(castlesOwner);
+        var secondCastle = GetCastle(castlesOwner);
+        board[1, 1].AddContent(firstCastle);
+        board[4, 4].AddContent(secondCastle);
+        var checker = new GameOverChecker(board);
+        
+        Assert.True(checker.IsGameOver());
+    }
+
+    [Fact]
+    public void GetWinner_ShouldReturnNullPlayer_IfGameIsNotOver()
+    {
+        var board = GetFullNByNBoard(10);
+        var firstCastle = GetCastle(GetPlayer());
+        var secondCastle = GetCastle(GetPlayer());
+        board[1, 1].AddContent(firstCastle);
+        board[4, 4].AddContent(secondCastle);
+        var checker = new GameOverChecker(board);
+        
+        Assert.True(checker.GetWinner().IsNull);
+    }
+    
+    [Fact]
+    public void GetWinner_ShouldReturnCastlesOwner_IfOneCastlesOwnerOnBoard()
+    {
+        var board = GetFullNByNBoard(10);
+        var castlesOwner = GetPlayer();
+        var firstCastle = GetCastle(castlesOwner);
+        var secondCastle = GetCastle(castlesOwner);
+        board[1, 1].AddContent(firstCastle);
+        board[4, 4].AddContent(secondCastle);
+        var checker = new GameOverChecker(board);
+        
+        Assert.Same(castlesOwner, checker.GetWinner());
+    }
+}
---
+++ castledice-game-logic/GameOverChecker.cs
@@ -0,0 +1,34 @@
+﻿using castledice_game_logic.GameObjects;
+
+namespace castledice_game_logic;
+
+public class GameOverChecker
+{
+    private Board _board;
+
+    public GameOverChecker(Board board)
+    {
+        _board = board;
+    }
+
+    public Player GetWinner()
+    {
+        return null;
+    }
+    
+    public bool IsGameOver(){
+        return false;
+    }
+
+    private List<Player> GetCastlesOwners()
+    {
+        var castlesOwners = new List<Player>();
+        return castlesOwners;
+    }
+
+    private List<Castle> GetCastles()
+    {
+        var castles = new List<Castle>();
+        return castles;
+    }
+}
