7bf131cbbe60e594943ca117e1abfb5a8126a49d
Mehanari <102820106+Mehanari@users.noreply.github.com>
2023-09-10 18:05:05 +0300

    Wrote stub for PassPenalty and added event to PlayerActionPoints.

---
+++ castledice-game-logic/Penalties/PassPenalty.cs
@@ -0,0 +1,25 @@
+﻿using castledice_game_logic.TurnsLogic;
+
+namespace castledice_game_logic.Penalties;
+
+/// <summary>
+/// This penalty is applied if player passes his turn several times in a row.
+/// Turn is considered to be passed if player didn't spend any action points.
+/// </summary>
+public class PassPenalty : IPenalty
+{
+    public List<Player> GetViolators()
+    {
+        throw new NotImplementedException();
+    }
+
+    public PassPenalty(int maxPassCount, TimeCondition timeCondition, ICurrentPlayerProvider currentPlayerProvider)
+    {
+        
+    }
+
+    public int GetPassCount(Player player)
+    {
+        return 0;
+    }
+}
