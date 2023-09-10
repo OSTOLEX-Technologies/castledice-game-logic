0bf226a068c3503600c42c63b01a2eca637c46e4
Mehanari <102820106+Mehanari@users.noreply.github.com>
2023-09-10 21:44:27 +0300

    Implemented GameOverChecker.

--- castledice-game-logic/GameOverChecker.cs
+++ castledice-game-logic/GameOverChecker.cs
@@ -13,22 +13,42 @@ public class GameOverChecker
 
     public Player GetWinner()
     {
-        return null;
+        var castlesOwners = GetCastlesOwners();
+        if (castlesOwners.Count == 1)
+        {
+            return castlesOwners[0];
+        }
+        return new NullPlayer();
     }
     
     public bool IsGameOver(){
-        return false;
+        return GetCastlesOwners().Count == 1;
     }
 
     private List<Player> GetCastlesOwners()
     {
-        var castlesOwners = new List<Player>();
-        return castlesOwners;
+        var castlesOwners = new HashSet<Player>();
+        var castles = GetCastles();
+        foreach (var castle in castles)
+        {
+            castlesOwners.Add(castle.GetOwner());
+        }
+        return castlesOwners.ToList();
     }
 
     private List<Castle> GetCastles()
     {
         var castles = new List<Castle>();
+        foreach (var cell in _board)
+        {
+            foreach (var content in cell.GetContent())
+            {
+                if (content is Castle castle)
+                {
+                    castles.Add(castle);
+                }
+            }
+        }
         return castles;
     }
 }
