e7b86e0f2b587be6170f855ce3329bee1952c0c9
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-10-04 21:30:41 +0300

    Added MatrixCellsGenerator and tested it.

---
+++ castledice-game-logic-tests/BoardGenerationTests/MatrixCellsGeneratorTests.cs
@@ -0,0 +1,56 @@
+﻿using castledice_game_logic;
+using castledice_game_logic.BoardGeneration.CellsGeneration;
+
+namespace castledice_game_logic_tests;
+
+public class MatrixCellsGeneratorTests
+{
+    [Theory]
+    [MemberData(nameof(MatrixCases))]
+    public void GenerateCells_ShouldSpawnCells_AccordingToMatrix(bool[,] matrix)
+    {
+        var board = new Board(CellType.Square);
+        var generator = new MatrixCellsGenerator(matrix);
+        
+        generator.GenerateCells(board);
+
+        for (int i = 0; i < board.GetLength(0); i++)
+        {
+            for (int j = 0; j < board.GetLength(1); j++)
+            {
+                Assert.Equal(matrix[i, j], board.HasCell(i, j));
+            }
+        }
+    }
+
+    public static IEnumerable<object[]> MatrixCases()
+    {
+        yield return new object[]
+        {
+            new bool[,]
+            {
+                { true, true, true },
+                { true, true, true },
+                { true, true, true, }
+            }
+        };
+        yield return new object[]
+        {
+            new bool[,]
+            {
+                { false, true, false },
+                { true, true, true },
+                { false, true, false, }
+            }
+        };
+        yield return new object[]
+        {
+            new bool[,]
+            {
+                { false, false, false },
+                { false, false, false },
+                { false, false, false, }
+            }
+        };
+    }
+}
---
+++ castledice-game-logic/BoardGeneration/CellsGeneration/MatrixCellsGenerator.cs
@@ -0,0 +1,30 @@
+﻿namespace castledice_game_logic.BoardGeneration.CellsGeneration;
+
+/// <summary>
+/// <inheritdoc />
+/// This one generates cells according to the given presence matrix: two dimensional boolean array where true means that there is cell on this place and false means that there is no cell on this place.
+/// Place in this context is a combination of indices in the matrix.
+/// </summary>
+public class MatrixCellsGenerator : ICellsGenerator
+{
+    private readonly bool[,] _presenceMatrix;
+
+    public MatrixCellsGenerator(bool[,] presenceMatrix)
+    {
+        _presenceMatrix = presenceMatrix;
+    }
+    
+    public void GenerateCells(Board board)
+    {
+        for (int i = 0; i < _presenceMatrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < _presenceMatrix.GetLength(1); j++)
+            {
+                if (_presenceMatrix[i, j])
+                {
+                    board.AddCell(i, j);
+                }
+            }
+        }
+    }
+}
