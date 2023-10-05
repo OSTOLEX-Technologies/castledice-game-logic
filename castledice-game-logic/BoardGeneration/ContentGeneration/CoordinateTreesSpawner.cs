a31ad92c49421970c019628adfae7cabdec1a72d
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-10-05 18:43:48 +0300

    Wrote CoordinateTreesSpawner class.

--- castledice-game-logic/BoardGeneration/ContentGeneration/CoordinateTreesSpawner.cs
+++ castledice-game-logic/BoardGeneration/ContentGeneration/CoordinateTreesSpawner.cs
@@ -5,13 +5,64 @@ namespace castledice_game_logic.BoardGeneration.ContentGeneration;
 
 public class CoordinateTreesSpawner : IContentSpawner
 {
+    private readonly List<Vector2Int> _coordinates;
+    private readonly ITreesFactory _factory;
+    
     public CoordinateTreesSpawner(List<Vector2Int> coordinates, ITreesFactory treesFactory)
     {
-        
+        if (CoordinatesListHasDuplicates(coordinates))
+        {
+            throw new ArgumentException("Coordinate list must not contain duplicates.");
+        }
+        if (CoordinatesListHasNegativeCoordinates(coordinates))
+        {
+            throw new ArgumentException("All coordinates must be non-negative.");
+        }
+
+        _coordinates = coordinates;
+        _factory = treesFactory;
+    }
+
+    private static bool CoordinatesListHasDuplicates(List<Vector2Int> coordinates)
+    {
+        for (int i = 0; i < coordinates.Count; i++)
+        {
+            var firstCoordinate = coordinates[i];
+            for (int j = i; j < coordinates.Count; j++)
+            {
+                if (j == i)
+                {
+                    continue;
+                }
+
+                var secondCoordinate = coordinates[j];
+                if (firstCoordinate == secondCoordinate)
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private static bool CoordinatesListHasNegativeCoordinates(List<Vector2Int> coordinates)
+    {
+        return coordinates.Exists(cord => cord.X < 0 || cord.Y < 0);
     }
 
     public void SpawnContent(Board board)
     {
-        throw new NotImplementedException();
+        foreach (var coordinate in _coordinates)
+        {
+            if (!board.HasCell(coordinate))
+            {
+                throw new ArgumentException("Board doesn't have cell on coordinate " + coordinate);
+            }
+        }
+        foreach (var coordinate in _coordinates)
+        {
+            board[coordinate].AddContent(_factory.GetTree());
+        }
     }
 }
