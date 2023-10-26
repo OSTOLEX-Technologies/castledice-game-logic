ec7aa7f3d18f463b114d12d0726b89e916b86116
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-10-26 17:08:57 +0300

    Added new tests for CoordinateContentSpawner.

--- castledice-game-logic-tests/BoardGenerationTests/CoordinateContentSpawnerTests.cs
+++ castledice-game-logic-tests/BoardGenerationTests/CoordinateContentSpawnerTests.cs
@@ -47,4 +47,70 @@ public class CoordinateContentSpawnerTests
             GetFullNByNBoard(6)
         };
     }
+    
+    [Theory]
+    [MemberData(nameof(InvalidBoardTestCases))]
+    public void SpawnContent_ShouldThrowArgumentException_IfGivenBoardHasNoCellsOnCoordinates(
+        List<ContentToCoordinate> contentToCoordinates, Board board)
+    {
+        var spawner = new CoordinateContentSpawner(contentToCoordinates);
+        
+        Assert.Throws<ArgumentException>(() => spawner.SpawnContent(board));
+    }
+
+    public static IEnumerable<object[]> InvalidBoardTestCases()
+    {
+        yield return new object[]
+        {
+            new List<ContentToCoordinate>
+            {
+                new ((10, 10), GetCellContent()),
+            },
+            GetFullNByNBoard(3)
+        };
+        
+        var boardWithAbsentCells = new Board(CellType.Square);
+        boardWithAbsentCells.AddCell(0, 0);
+        boardWithAbsentCells.AddCell(1, 2);
+        yield return new object[]
+        {
+            new List<ContentToCoordinate>
+            {
+                new((0, 0), GetCellContent()),
+                new((2, 1), GetCellContent())
+            }
+        };
+    }
+
+    [Theory]
+    [MemberData(nameof(InvalidCoordinatesTestCases))]
+    public void Constructor_ShouldThrowArgumentException_IfGivenCoordinatesAreInvalid(List<ContentToCoordinate> invalidContentToCoordinates)
+    {
+        Assert.Throws<ArgumentException>(() => new CoordinateContentSpawner(invalidContentToCoordinates));
+    }
+    
+    public static IEnumerable<object[]> InvalidCoordinatesTestCases()
+    {
+        yield return new object[]
+        {
+            new List<ContentToCoordinate>
+            {
+                new ((-1, 0), GetCellContent()),
+                new ((0, -2), GetCellContent()),
+                new ((-3, 0), GetCellContent()),
+                new ((0, 0), GetCellContent())
+            }
+        };
+        
+        yield return new object[]
+        {
+            new List<ContentToCoordinate>
+            {
+                new ((0, 0), GetCellContent()),
+                new ((1, 1), GetCellContent()),
+                new ((2, -4), GetCellContent()),
+                new ((0, 0), GetCellContent())
+            }
+        };
+    }
 }
