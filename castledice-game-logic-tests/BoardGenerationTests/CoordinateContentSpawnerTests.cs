0ebd00c96dc1e00ea2079a81391e3734cddd5ceb
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-10-26 17:03:07 +0300

    Wrote test cases for first test of CoordinateContentSpawner.

--- castledice-game-logic-tests/BoardGenerationTests/CoordinateContentSpawnerTests.cs
+++ castledice-game-logic-tests/BoardGenerationTests/CoordinateContentSpawnerTests.cs
@@ -1,10 +1,13 @@
 ﻿using castledice_game_logic;
 using castledice_game_logic.BoardGeneration.ContentGeneration;
+using static castledice_game_logic_tests.ObjectCreationUtility;
 
 namespace castledice_game_logic_tests;
 
 public class CoordinateContentSpawnerTests
 {
+    [Theory]
+    [MemberData(nameof(SpawnContentTestCases))]
     public void SpawnContent_ShouldAddGivenContent_ToCellsWithGivenCoordinates(
         List<ContentToCoordinate> contentToCoordinates, Board board)
     {
@@ -17,5 +20,31 @@ public class CoordinateContentSpawnerTests
             var cell = board[contentToCoordinate.Coordinate];
             Assert.Contains(contentToCoordinate.Content, cell.GetContent());
         }
+    }
+
+    public static IEnumerable<object[]> SpawnContentTestCases()
+    {
+        yield return new object[]
+        {
+            new List<ContentToCoordinate>
+            {
+                new ((0, 0), GetCellContent()),
+                new ((1, 1), GetCellContent()),
+                new ((2, 2), GetCellContent()),
+                new ((3, 3), GetCellContent())
+            },
+            GetFullNByNBoard(5)
+        };
+        
+        yield return new object[]
+        {
+            new List<ContentToCoordinate>
+            {
+                new ((0, 1), GetCellContent()),
+                new ((2, 5), GetCellContent()),
+                new ((1, 3), GetCellContent())
+            },
+            GetFullNByNBoard(6)
+        };
     }
 }
