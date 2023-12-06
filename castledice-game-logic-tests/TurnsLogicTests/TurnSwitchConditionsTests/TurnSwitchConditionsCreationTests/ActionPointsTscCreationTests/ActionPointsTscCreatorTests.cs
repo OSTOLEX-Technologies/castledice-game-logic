0ad462063848b4fff946ff0ace4c61f61d2da757
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-12-06 20:40:51 +0200

    Tested and implemented ActionPointsTscCreator.

---
+++ castledice-game-logic-tests/TurnsLogicTests/TurnSwitchConditionsTests/TurnSwitchConditionsCreationTests/ActionPointsTscCreationTests/ActionPointsTscCreatorTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using castledice_game_logic.TurnsLogic;
+using castledice_game_logic.TurnsLogic.TurnSwitchConditions.TurnSwitchConditionsCreation.ActionPointsTscCreation;
+using Moq;
+
+namespace castledice_game_logic_tests.TurnsLogicTests.TurnSwitchConditionsTests.TurnSwitchConditionsCreationTests.ActionPointsTscCreationTests;
+
+public class ActionPointsTscCreatorTests
+{
+    [Fact]
+    public void Create_ShouldReturnActionPointsTsc_WithCurrentPlayerProviderFromConstructor()
+    {
+        var currentPlayerProvider = new Mock<ICurrentPlayerProvider>().Object;
+        var creator = new ActionPointsTscCreator(currentPlayerProvider);
+        
+        var actualTsc = creator.CreateActionPointsTsc();
+        var fieldInfo = actualTsc.GetType().GetField("_currentPlayerProvider", BindingFlags.NonPublic | BindingFlags.Instance);
+        var actualCurrentPlayerProvider = fieldInfo.GetValue(actualTsc);
+        
+        Assert.Same(currentPlayerProvider, actualCurrentPlayerProvider);
+    }
+}
---
+++ castledice-game-logic/TurnsLogic/TurnSwitchConditions/TurnSwitchConditionsCreation/ActionPointsTscCreation/ActionPointsTscCreator.cs
@@ -0,0 +1,16 @@
+﻿namespace castledice_game_logic.TurnsLogic.TurnSwitchConditions.TurnSwitchConditionsCreation.ActionPointsTscCreation;
+
+public class ActionPointsTscCreator : IActionPointsTscCreator
+{
+    private readonly ICurrentPlayerProvider _currentPlayerProvider;
+
+    public ActionPointsTscCreator(ICurrentPlayerProvider currentPlayerProvider)
+    {
+        _currentPlayerProvider = currentPlayerProvider;
+    }
+
+    public ActionPointsTsc CreateActionPointsTsc()
+    {
+        return new ActionPointsTsc(_currentPlayerProvider);
+    }
+}
