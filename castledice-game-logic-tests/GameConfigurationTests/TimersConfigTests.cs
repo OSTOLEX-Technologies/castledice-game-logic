4ac4c04883601e949b95826b756aaac660cdd161
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-12-27 01:43:53 +0200

    Tested and implemented TimersConfig.

---
+++ castledice-game-logic-tests/GameConfigurationTests/TimersConfigTests.cs
@@ -0,0 +1,20 @@
+﻿using castledice_game_logic.GameConfiguration;
+
+namespace castledice_game_logic_tests.GameConfigurationTests;
+
+public class TimersConfigTests
+{
+    [Fact]
+    public void GetTimeSpanForPlayer_ShouldReturnTimeSpan_GivenInConstructor()
+    {
+        var rnd = new Random();
+        var playerId = rnd.Next();
+        var expectedTimeSpan = TimeSpan.FromSeconds(rnd.Next());
+        var playerIdToTimeSpan = new Dictionary<int, TimeSpan> {{playerId, expectedTimeSpan}};
+        var timersConfig = new TimersConfig(playerIdToTimeSpan);
+        
+        var actualTimeSpan = timersConfig.GetTimeSpanForPlayer(playerId);
+        
+        Assert.Equal(expectedTimeSpan, actualTimeSpan);
+    }
+}
---
+++ castledice-game-logic/GameConfiguration/TimersConfig.cs
@@ -0,0 +1,16 @@
+﻿namespace castledice_game_logic.GameConfiguration;
+
+public class TimersConfig
+{
+    private readonly Dictionary<int, TimeSpan> _playerIdToTimeSpan;
+
+    public TimersConfig(Dictionary<int, TimeSpan> playerIdToTimeSpan)
+    {
+        _playerIdToTimeSpan = playerIdToTimeSpan;
+    }
+    
+    public TimeSpan GetTimeSpanForPlayer(int playerId)
+    {
+        return _playerIdToTimeSpan[playerId];
+    }
+}
